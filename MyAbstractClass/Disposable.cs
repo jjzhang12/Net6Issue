eac285bc2e9ae226f15afc2550a6d5a8797cc251
JJ Zhang <jj.zhang@aveva.com>
2021-10-14 14:00:00 -0700

    Initial

---
+++ MyAbstractClass/Disposable.cs
@@ -0,0 +1,36 @@
+﻿namespace MyAbstractClass
+{
+    using System;
+    public abstract class Disposable : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Disposable"/> class.
+        /// </summary>
+        protected Disposable()
+        {
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            try
+            {
+                this.Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+    }
+}
---
+++ MyClass/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyAbstractClass;
+
+namespace MyClass
+{
+    public class Class1 : Disposable
+    {
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
